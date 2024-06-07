2b499a6dd5748d318813053ced34451b86f248fd
niamul007 <am3994566@gmail.com>
2024-06-07 21:09:58 +0600

    add all

--- src/components/Data.jsx
+++ src/components/Data.jsx
@@ -49,60 +49,60 @@ export const data = [
     time: "GELLO DOE | March 26 2021 | 260 Views",
     img:"post1.jpeg"
   },
-  {
-    id: 6,
-    tag:"Technology",
-    title: "Plan Your Work and Work Your Plan",
-    author: "Gello Due",
-    time: "GELLO DOE | March 25, 2021 | 113 Views",
-    detail:"Venus has a runaway greenhouse effect. I kind of want to know what happened there because we’re twirling knobs here on Earth without knowing the consequences of it. Mars once",
-    img:"post5.jpeg"
-  },
-  {
-    id: 7,
-    tag:"Food",
-    title: "The Most Popular Fast Food Chains",
-    author: "Gello Due",
-    time: "GELLO DOE | March 25 2021 | 129 Views",
-    detail:"Venus has a runaway greenhouse effect. I kind of want to know what happened there because we’re twirling knobs here on Earth without knowing the consequences of it. Mars once",
-    img:"d.jpg"
-  },
-  {
-    id: 8,
-    tag:"Sports",
-    title: "Most Watched Sports in the World",
-    author: "Gello Due",
-    time: "GELLO DOE | March 25, 2021 | 129 Views",
-    img:"car.jpg",
-    detail:"Venus has a runaway greenhouse effect. I kind of want to know what happened there because we’re twirling knobs here on Earth without knowing the consequences of it. Mars once"
-  },
-  {
-    id: 9,
-    tag:"Sports",
-    title: "Benefits of Jogging to Stay Healthy",
-    author: "Gello Due",
-    time: "GELLO DOE | March 25, 2021 | 129 Views",
-    img:"post8.jpeg",
-    detail:"Venus has a runaway greenhouse effect. I kind of want to know what happened there because we’re twirling knobs here on Earth without knowing the consequences of it. Mars once"
-  },
-  {
-    id: 10,
-    tag:"Technology",
-    title: "The Incredible Thing About Virtual Reality",
-    author: "Gello Due",
-    time: "GELLO DOE | March 25, 2021 | 129 Views",
-    img:"post9.jpeg",
-    detail:"Venus has a runaway greenhouse effect. I kind of want to know what happened there because we’re twirling knobs here on Earth without knowing the consequences of it. Mars once"
+  // {
+  //   id: 6,
+  //   tag:"Technology",
+  //   title: "Plan Your Work and Work Your Plan",
+  //   author: "Gello Due",
+  //   time: "GELLO DOE | March 25, 2021 | 113 Views",
+  //   detail:"Venus has a runaway greenhouse effect. I kind of want to know what happened there because we’re twirling knobs here on Earth without knowing the consequences of it. Mars once",
+  //   img:"post5.jpeg"
+  // },
+  // {
+  //   id: 7,
+  //   tag:"Food",
+  //   title: "The Most Popular Fast Food Chains",
+  //   author: "Gello Due",
+  //   time: "GELLO DOE | March 25 2021 | 129 Views",
+  //   detail:"Venus has a runaway greenhouse effect. I kind of want to know what happened there because we’re twirling knobs here on Earth without knowing the consequences of it. Mars once",
+  //   img:"d.jpg"
+  // },
+  // {
+  //   id: 8,
+  //   tag:"Sports",
+  //   title: "Most Watched Sports in the World",
+  //   author: "Gello Due",
+  //   time: "GELLO DOE | March 25, 2021 | 129 Views",
+  //   img:"car.jpg",
+  //   detail:"Venus has a runaway greenhouse effect. I kind of want to know what happened there because we’re twirling knobs here on Earth without knowing the consequences of it. Mars once"
+  // },
+  // {
+  //   id: 9,
+  //   tag:"Sports",
+  //   title: "Benefits of Jogging to Stay Healthy",
+  //   author: "Gello Due",
+  //   time: "GELLO DOE | March 25, 2021 | 129 Views",
+  //   img:"post8.jpeg",
+  //   detail:"Venus has a runaway greenhouse effect. I kind of want to know what happened there because we’re twirling knobs here on Earth without knowing the consequences of it. Mars once"
+  // },
+  // {
+  //   id: 10,
+  //   tag:"Technology",
+  //   title: "The Incredible Thing About Virtual Reality",
+  //   author: "Gello Due",
+  //   time: "GELLO DOE | March 25, 2021 | 129 Views",
+  //   img:"post9.jpeg",
+  //   detail:"Venus has a runaway greenhouse effect. I kind of want to know what happened there because we’re twirling knobs here on Earth without knowing the consequences of it. Mars once"
 
-  },
-  {
-    id: 11,
-    tag:"Fashion",
-    title: "The Trend of Women’s Clothing",
-    author: "Gello Due",
-    time: "GELLO DOE | March 25, 2021 | 129 Views",
-    img:"post8.jpeg",
-    detail:"Venus has a runaway greenhouse effect. I kind of want to know what happened there because we’re twirling knobs here on Earth without knowing the consequences of it. Mars once"
+  // },
+  // {
+  //   id: 11,
+  //   tag:"Fashion",
+  //   title: "The Trend of Women’s Clothing",
+  //   author: "Gello Due",
+  //   time: "GELLO DOE | March 25, 2021 | 129 Views",
+  //   img:"post8.jpeg",
+  //   detail:"Venus has a runaway greenhouse effect. I kind of want to know what happened there because we’re twirling knobs here on Earth without knowing the consequences of it. Mars once"
 
-  },
+  // },
 ];
--- src/components/Edited.jsx
+++ src/components/Edited.jsx
@@ -1,76 +1,125 @@
-import React from 'react'
+import React, { useEffect, useRef } from "react";
 
 function Edited(props) {
   return (
-    <div className={`container ${props.darkMode? "dark" :''}`}>
+      <div className={`container ${props.darkMode ? "dark" : ""}`}>
         <div className="e-post">
-            <h2 className={`edited-text ${props.darkMode? "dark" :''}`}>Edited posts____</h2>
-            <img src="post2.jpeg" alt="" className="e-img" />
-            <div className="detail">
-                <p className="e-tag">Cars</p>
-                <p className={`e-title ${props.darkMode? "dark" :''}`}>
-                Review of New Porsche Cars
-                </p>
-            </div>
+          <h2 className={`edited-text ${props.darkMode ? "dark" : ""}`}>
+            Edited posts____
+          </h2>
+          <img src="post2.jpeg" alt="" className="e-img" />
+          <div className="detail">
+            <p className="e-tag">Cars</p>
+            <p className={`e-title ${props.darkMode ? "dark" : ""}`}>
+              Review of New Porsche Cars
+            </p>
+          </div>
         </div>
         <div className="e-post">
-            <img src="post1.jpeg" alt="" className="e-img" />
-            <div className="detail">
-                <p className="e-tag">Fashion</p>
-                <p className={`e-title ${props.darkMode? "dark" :''}`}>
-                How Lovely Yellow is! It Stands for the Sun
-                </p>
-            </div>
+          <img src="post1.jpeg" alt="" className="e-img" />
+          <div className="detail">
+            <p className="e-tag">Fashion</p>
+            <p className={`e-title ${props.darkMode ? "dark" : ""}`}>
+              How Lovely Yellow is! It Stands for the Sun
+            </p>
+          </div>
         </div>
         <div className="e-post">
-            <img src="post4.jpeg" alt="" className="e-img" />
-            <div className="detail">
-                <p className="e-tag">Technology</p>
-                <p className={`e-title ${props.darkMode? "dark" :''}`}>
-                Style Tips Every Woman Should Know
-                </p>
-            </div>
+          <img src="post4.jpeg" alt="" className="e-img" />
+          <div className="detail">
+            <p className="e-tag">Technology</p>
+            <p className={`e-title ${props.darkMode ? "dark" : ""}`}>
+              Style Tips Every Woman Should Know
+            </p>
+          </div>
         </div>
         <div className="e-post">
-            <img src="d.jpg" alt="" className="e-img" />
-            <div className="detail">
-                <p className="e-tag">Food</p>
-                <p className={`e-title ${props.darkMode? "dark" :''}`}>
-                The Most Popular Fast Food Chains </p>
-            </div>
+          <img src="d.jpg" alt="" className="e-img" />
+          <div className="detail">
+            <p className="e-tag">Food</p>
+            <p className={`e-title ${props.darkMode ? "dark" : ""}`}>
+              The Most Popular Fast Food Chains{" "}
+            </p>
+          </div>
         </div>
         <div className="joinContainer">
-            <h2 className="join-us">
-                Join Us_____
+          <h2 className={`join-us ${props.darkMode ? "dark" : ""}`}>
+            Join Us_____
+          </h2>
+          <a href="#" className="icon-container">
+            <i
+              class={`fa fa-facebook ${props.darkMode ? "dark" : ""}`}
+              aria-hidden="true"
+            ></i>
+            <p className={`i-name ${props.darkMode ? "dark" : ""}`}>
+              Facebook 20k
+            </p>
+            <p className={`left-i ${props.darkMode ? "dark" : ""}`}>➡</p>
+          </a>
+          <a href="#" className="icon-container">
+            <i
+              class={`fa fa-twitter ${props.darkMode ? "dark" : ""}`}
+              aria-hidden="true"
+            ></i>
+            <p className={`i-name ${props.darkMode ? "dark" : ""}`}>
+              Twitter 200k
+            </p>
+            <p className={`left-i ${props.darkMode ? "dark" : ""}`}>➡</p>
+          </a>
+          <a href="#" className="icon-container">
+            <i
+              class={`fa fa-linkedin ${props.darkMode ? "dark" : ""}`}
+              aria-hidden="true"
+            ></i>
+            <p className={`i-name ${props.darkMode ? "dark" : ""}`}>
+              Linkedin 10k
+            </p>
+            <p className={`left-i ${props.darkMode ? "dark" : ""}`}>➡</p>
+          </a>
+          <a href="#" className="icon-container">
+            <i
+              class={`fa fa-instagram ${props.darkMode ? "dark" : ""}`}
+              aria-hidden="true"
+            ></i>
+            <p className={`i-name ${props.darkMode ? "dark" : ""}`}>
+              Instagram 30k
+            </p>
+            <p className={`left-i ${props.darkMode ? "dark" : ""}`}>➡</p>
+          </a>
+          <a href="#" className="icon-container">
+            <i
+              class={`fa fa-github ${props.darkMode ? "dark" : ""}`}
+              aria-hidden="true"
+            ></i>
+            <p className={`i-name ${props.darkMode ? "dark" : ""}`}>
+              Github 6k
+            </p>
+            <p className={`left-i ${props.darkMode ? "dark" : ""}`}>➡</p>
+          </a>
+
+          <div className="news-shelter">
+            <h2 className={`news ${props.darkMode ? "dark" : ""}`}>
+              Newsletter____
             </h2>
-            <a href='#' className="icon-container">
-                <i class="fa fa-facebook " aria-hidden="true"></i>
-                <p className="i-name">Facebook 20k</p>
-                <p className="left-i">➡</p>
-            </a>
-            <a href='#' className="icon-container">
-            <i class="fa fa-twitter" aria-hidden="true"></i>
-                <p className="i-name">Twitter 200k</p>
-                <p className="left-i">➡</p>
-            </a>
-            <a href='#' className="icon-container">
-            <i class="fa fa-linkedin" aria-hidden="true"></i>
-                <p className="i-name">Linkedin 10k</p>
-                <p className="left-i">➡</p>
-            </a>
-            <a href='#' className="icon-container">
-            <i class="fa fa-instagram" aria-hidden="true"></i>
-                <p className="i-name">Instagram 30k</p>
-                <p className="left-i">➡</p>
-            </a>
-            <a href='#' className="icon-container">
-            <i class="fa fa-github" aria-hidden="true"></i>
-                <p className="i-name">Github 6k</p>
-                <p className="left-i">➡</p>
-            </a>
+            <p className={`text ${props.darkMode ? "dark" : ""}`}>
+              Get the latest creative news from Gello magazine
+            </p>
+            <div className={`sign-up ${props.darkMode ? "dark" : ""}`}>
+              <input
+                type="email"
+                name=""
+                id=""
+                className={`email ${props.darkMode ? "dark" : ""}`}
+                placeholder="Your email address"
+              />
+              <button className={`sign-up-btn ${props.darkMode ? "dark" : ""}`}>
+                Sign Up
+              </button>
+            </div>
+          </div>
         </div>
-    </div>
-  )
+      </div>
+  );
 }
 
-export default Edited
+export default Edited;
